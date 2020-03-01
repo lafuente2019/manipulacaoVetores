5980799103731ad7ebcb8c4a1ee48d6dca71b774
Valter Lafuente Junior <valterlafuente@yahoo.com.br>
2020-03-01 17:55:37 -0300

    acrescentando a função intercalaVetor

--- src/manipulacaoVetores.java
+++ src/manipulacaoVetores.java
@@ -3,6 +3,25 @@ import java.util.Scanner;
 
 public class manipulacaoVetores {
 	
+	public static int tamanhoVetor() {
+		Scanner ler = new Scanner(System.in);
+		System.out.println("Digite o tamanho do seu vetor: ");
+		int tm = ler.nextInt();
+		if (tm > 0 && tm <= 100) {
+			return tm;
+		}else {
+			return -1;	
+		}
+		
+	}
+	public static void populaVetor(int []vetor) {
+		Scanner ler = new Scanner(System.in);
+		for (int i = 0; i < vetor.length; i++) {
+			System.out.println("Vetor [" + i +"]:");
+			vetor[i] = ler.nextInt();
+			System.out.println("-------------------");
+		}
+	}
 	 public static int Maior(int v[], int elementos) {
 
 	        int max = 0;
@@ -13,7 +32,6 @@ public class manipulacaoVetores {
 	        }
 	        return max;
 	    }
-	 
 	 public static void Inserir(int vetor[], int elementos, int posicao) {
 		 if (posicao >= 0 && posicao <= elementos) {
              for (int i = elementos-1; i >= posicao; i--) {
@@ -28,6 +46,38 @@ public class manipulacaoVetores {
 	        for (int i = posicao; i < elementos - 1; i++) {
 	            v[i] = v[i + 1];
 	        }
+	    }
+	  public static void intercalaVetor(int v[],int[]v1,int v2[]) {
+		  int aux = 0;
+	        for (int i = 0; i < v.length; i++) {
+	            v2[aux] = v[i];
+	            aux += 2;
+
+	        }
+	        aux = 1;
+	        for (int i = 0; i < v1.length; i++) {
+	            v2[aux] = v1[i];
+	            aux += 2;
+	        }
+	       System.out.println("----Criando o vetor C com a soma "
+	               + "do tamanhos de A e B -----");
+	       for (int i = 0; i < v2.length; i++) {
+	        System.out.println("vetor[" + i + "]" + v2[i]);
+	      }
+	        System.out.println("----Ordem crescente-----");
+	       int p;
+	       for (int i = 0; i < v2.length ; i++) {
+	            for (int j = 0 ; j < v2.length-1 ; j++) {
+	                if(v2[i] < v2[j]){
+	                 p = v2[i];
+	                 v2[i] = v2[j];
+	                 v2[j] = p; 
+	                }  
+	            }    
+	        }
+	        for (int i = 0; i < v2.length; i++) {
+	        System.out.println("vetor[" + i + "]" + v2[i]);
+	      }
 	    }
 	public static void main(String[] args) {
 		
